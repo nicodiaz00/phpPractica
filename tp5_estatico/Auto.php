017c6645b34f6a6d3bef0b1f1d415472c7865bea
nicodiaz00 <ngdiaz00@gmail.com>
2024-06-03 12:34:44 -0300

    practicando stactic

--- tp5_estatico/Auto.php
+++ tp5_estatico/Auto.php
@@ -2,12 +2,18 @@
 
 class Auto
 {
+    private static int $cantidad =0; //propiedad estatica
+    private int $id; //atributo que sera una IDENTIDAD
     private string $color;
     private int $cantRuedas;
     private int $velocidad;
 
     protected string $marca;
     protected string $modelo;
+
+    public function getId(){
+        return $this->id;
+}
 
     public function getColor(){
         return $this->color;
@@ -19,6 +25,10 @@ class Auto
         return $this->velocidad;
     }
 
+    public function setId(int $unId)
+    {
+        $this->id = $unId;
+    }
     public function setColor(string $color){
         $this->color = $color;
     }
@@ -28,8 +38,17 @@ class Auto
     public function setVelocidad(int $velocidad){
         $this->velocidad = $velocidad;
     }
+    public static function crear(){
+         $nuevo =new Auto();
+         Auto::$cantidad = Auto::$cantidad +1;
+         $nuevo->setId(Auto::$cantidad);
+         return $nuevo;
+    }
 
-    public function __construct(string $autoMarca, string $autoModelo,string $colorAuto, int $cantDeRuedas)
+
+
+
+    public function __construct(string $autoMarca="", string $autoModelo="",string $colorAuto="", int $cantDeRuedas=4)
     {
         //$this->cantRuedas = $cantDeRuedas;
         //$this->color = $colorAuto;
@@ -42,6 +61,8 @@ class Auto
 
 
     }
+
+    //esta funcion compara un auto con otro y devuelve true o false.
     public function iguales(Auto $otroAuto)
     {
         if($this->getColor() === $otroAuto->getColor() && $this->marca === $otroAuto->marca && $this->modelo === $otroAuto->modelo){
@@ -52,6 +73,7 @@ class Auto
 
         }
     }
+
 
 }
 
--- tp5_estatico/test00.php
+++ tp5_estatico/test00.php
@@ -7,12 +7,18 @@ include_once ('Helpers.php');
  $autito1 = new Auto("Ford","Ka","Rojo",4);
  $autito2 = new Auto("Ford","Ka","Rojo",4);
 
+$autito3 = Auto::crear();
+$autito4 = Auto::crear();
 
 
 
-  $tipo = $autito1->iguales($autito2);
+
+  // $tipo = $autito1->iguales($autito2);
 
 
 
-  echo $tipo;
-  echo igualdadDeAutos($tipo);
+  //echo $tipo;
+  //echo igualdadDeAutos($tipo);
+  echo $autito3->getId();
+  echo " ";
+  echo $autito4->getId();
